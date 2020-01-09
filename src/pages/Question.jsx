f15b861a2bdbc20b55e2eb7d6bfedb4c0499e8c1
Justin Swiger <justin.swiger@me.com>
2020-01-09 13:56:18 -0500

    updated onequestion page

--- src/pages/Question.jsx
+++ src/pages/Question.jsx
@@ -5,15 +5,9 @@ const Question = props => {
     <div>
       <ul>
         <li>
-<<<<<<< HEAD
           <h3>{props.description}</h3>
           <p>{props.content}</p>
           <p>{props.dateOfPost}</p>
-=======
-          <h3>Title: {props.description}</h3>
-          <p>Content: {props.content}</p>
-          <p>Date: {props.dateOfPost}</p>
->>>>>>> 2a0511a92dbef5b75b0d7ed8fc6b62fa5d6faaa6
           <div>
             <span>Upvotes: {props.upDownVoteQuestion}</span>
           </div>
